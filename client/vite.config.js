7df1e850c016c61614cb1bc5a91f45131506b4ef
blackcave0 <i3intel@protonmail.com>
2024-03-23 17:38:58 +0530

    a

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server :{
-    hmr : {overlay : false}
-  }
+    hmr : {overlay : false},
+    proxy : {
+      "/api" : {
+        target : 'https://s2m.netlify.app'
+      }
+    }
+  },
 })
