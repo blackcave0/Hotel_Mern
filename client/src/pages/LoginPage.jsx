3715082dbf370097e9d9b28ab0faa1497dfb8385
blackcave0 <i3intel@protonmail.com>
2024-03-23 17:16:12 +0530

    a

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -21,7 +21,7 @@ function LoginPage() {
     event.preventDefault();
     // console.log(user);
     try {
-      const response = await fetch(`Server/api/auth/login`, {
+      const response = await fetch(`api/auth/login`, {
         method : "POST", 
         headers : {
           "Content-Type" : "application/json",
